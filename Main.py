8a1f49c8f1b9f8b76f5fa37f33b5cc337d7c6220
Jura <jurakulek@gmail.com>
2018-11-13 17:43:03 +0200

    add attack animation and idle animation

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from cocos.director import director
 class Mover(cocos.actions.Move):
     def step(self,dt):
         super().step(dt)
-        vel_x = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * 500
+        vel_x = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * 180
         self.target.velocity = (vel_x, 0)
         scroller.set_focus(self.target.x, self.target.y)
 
@@ -18,21 +18,45 @@ class HelloCocos(cocos.layer.ScrollableLayer):
     is_event_handler = True
     def __init__(self):
         super().__init__()
-        #run right
-        img_r = pyglet.image.load('adventurer-run3-sword-Sheet.png')
-        img_grid_r = pyglet.image.ImageGrid(img_r, 1, 6, item_width=50, item_height=37 )
+        #run right --------------------------------------------------
+        self.img_r = pyglet.image.load('adventurer-run3-sword-Sheet.png')
+        self.img_grid_r = pyglet.image.ImageGrid(self.img_r, 1, 6, item_width=50, item_height=37 )
 
-        anim_r = pyglet.image.Animation.from_image_sequence(img_grid_r[0:], 0.1, loop=True)
-
-        self.spriterunright = cocos.sprite.Sprite(anim_r)
-        self.spriterunright.position = (100, 180)
-        self.spriterunright.scale = 2
-        self.spriterunright.scale_x = 1
-        self.spriterunright.velocity = (0,0)
-        self.spriterunright.do(Mover())
-        self.add(self.spriterunright)
+        self.anim_r = pyglet.image.Animation.from_image_sequence(self.img_grid_r[0:], 0.1, loop=True)
+        # ----------------------------------------------------------
 
         #attak1
+        self.img_a1 = pyglet.image.load('attack1/Attacksheet.png')
+        self.img_grid_a1 = pyglet.image.ImageGrid(self.img_a1, 1, 5, item_width=50, item_height=37 )
+
+        self.anim_a1 = pyglet.image.Animation.from_image_sequence(self.img_grid_a1[0:], 0.1, loop=True)
+        #_-----------------------------------------------------------------
+
+        #attak1
+        self.img_i = pyglet.image.load('idle/idlesheet.png')
+        self.img_grid_i = pyglet.image.ImageGrid(self.img_i, 1, 4, item_width=50, item_height=37 )
+
+        self.anim_i = pyglet.image.Animation.from_image_sequence(self.img_grid_i[0:], 0.3, loop=True)
+        #_-----------------------------------------------------------------
+
+
+        self.sprite = cocos.sprite.Sprite(self.anim_i)
+        self.sprite.position = (100, 180)
+        self.sprite.scale = 2
+        self.sprite.scale_x = 1
+        self.sprite.velocity = (0,0)
+        self.sprite.do(Mover())
+        self.add(self.sprite)
+        
+
+
+        
+
+
+
+
+
+
 
         self.speed = 100.0
         self.pressed = defaultdict(int)
@@ -40,13 +64,20 @@ class HelloCocos(cocos.layer.ScrollableLayer):
 
 
     def on_key_press(self, k, m):
+        print(k)
         if k == 65361:
-            self.spriterunright.scale_x = -1
+            self.sprite.scale_x = -1
+            self.sprite._animation = self.anim_r
         if k == 65363:
-            self.spriterunright.scale_x = 1
+            self.sprite.scale_x = 1
+            self.sprite._animation = self.anim_r
+
+        if k == 65362:
+            self.sprite._animation = self.anim_a1
+
 
     def on_key_release(self, k, m):
-        pass
+        self.sprite._animation = self.anim_i
 
 
     def update(self, dt):
