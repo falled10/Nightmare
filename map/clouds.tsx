ce452e0398b78a9827eb3966209604a615b5b26e
whateverfw <40571528+whateverfw@users.noreply.github.com>
2018-11-14 19:02:51 +0200

    Test on_new_game

---
+++ map/clouds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="clouds" tilewidth="60" tileheight="12" tilecount="56" columns="4">
+ <image source="../gothicvania patreon collection/Gothic-Horror-Files/PNG/layers/clouds.png" width="256" height="176"/>
+</tileset>
