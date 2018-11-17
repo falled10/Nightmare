855b739cdb4505403efef07fe14588a445d00bb0
whateverfw <40571528+whateverfw@users.noreply.github.com>
2018-11-17 19:35:13 +0200

    before pull wt

--- Level1_Layer.py
+++ Level1_Layer.py
@@ -1,14 +1,16 @@
 import cocos
+import pyglet
 from cocos.director import director
 from cocos.scene import Scene
-
-from cocos.layer import Layer
+from cocos.layer import Layer, ScrollableLayer, ScrollingManager
 from cocos.sprite import Sprite
+from cocos.actions import Move
+from collections import defaultdict
+from pyglet.window import key
 
-__all__ = ['get_newgame']
 
 
-class Level1_Layer(Layer):
+class Level1_Layer(ScrollableLayer):
     is_event_handler = True  #: enable director.window events
 
     def __init__(self):
@@ -26,7 +28,85 @@ class Level1_Layer(Layer):
     
 
 def get_newgame():
+  
     scene = Scene()
-    scene.add(Level1_Layer())
+    layer = MainHero()
+    level1_layer = Level1_Layer
+
+    scroller_1 = ScrollingManager()
+    scroller_1.add(level1_layer)
+    scroller_1.add(layer)
+
+    scene.add(scroller_1)
 
     return scene
+
+class MainHero(ScrollableLayer):   
+    is_event_handler = True
+    def __init__(self):
+        super().__init__()
+        #run right --------------------------------------------------
+        self.img_r = pyglet.image.load('adventurer-run3-sword-Sheet.png')
+        self.img_grid_r = pyglet.image.ImageGrid(self.img_r, 1, 6, item_width=50, item_height=37 )
+        self.anim_r = pyglet.image.Animation.from_image_sequence(self.img_grid_r[0:], 0.1, loop=True)
+        # ----------------------------------------------------------
+        
+        #attak1
+        self.img_a1 = pyglet.image.load('attack1/Attacksheet.png')
+        self.img_grid_a1 = pyglet.image.ImageGrid(self.img_a1, 1, 5, item_width=50, item_height=37 )
+
+        self.anim_a1 = pyglet.image.Animation.from_image_sequence(self.img_grid_a1[0:], 0.1, loop=True)
+        #_-----------------------------------------------------------------
+
+        # idle
+        self.img_i = pyglet.image.load('idle/idlesheet.png')
+        self.img_grid_i = pyglet.image.ImageGrid(self.img_i, 1, 4, item_width=50, item_height=37 )
+
+        self.anim_i = pyglet.image.Animation.from_image_sequence(self.img_grid_i[0:], 0.3, loop=True)
+        #_-----------------------------------------------------------------
+
+
+        self.sprite = Sprite(self.anim_i)
+        self.sprite.position = (100, 180)
+        self.sprite.scale = 2
+        self.sprite.scale_x = 1
+        self.sprite.velocity = (0,0)
+        self.sprite.do(Mover())
+        self.add(self.sprite)
+        
+
+        self.speed = 100.0
+        self.pressed = defaultdict(int)
+        self.schedule(self.update)
+        #h
+
+    def on_key_press(self, k, m):
+        print(k)
+
+        if k == 65361:
+            self.sprite.scale_x = -1
+            self.sprite._animation = self.anim_r
+
+        if k == 65363:
+            self.sprite.scale_x = 1
+            self.sprite._animation = self.anim_r
+
+        if k == 65362:
+            self.sprite._animation = self.anim_a1
+
+
+    def on_key_release(self, k, m):
+        self.sprite._animation = self.anim_i
+
+
+    def update(self, dt):
+        pass
+
+class Mover(Move):
+    def step(self,dt):
+        from Level1_Layer import get_newgame
+        super().step(dt)
+        vel_x = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * 180
+        self.target.velocity = (vel_x, 0)
+        scroller_1.set_focus(self.target.x, self.target.y)
+        # scroller_2.set_focus(self.target.x, self.target.y)
--- Main.py
+++ Main.py
@@ -6,15 +6,18 @@ from Menu import MainMenu
 from cocos.director import director
 from cocos.layer import *
 from Menu_Background import MainScene
-
+from pyglet.window import key
 
 def main():
     director.init(width=800, height=600, caption="Nightmare")
-
+    director.window.pop_handlers()
+    keyboard = key.KeyStateHandler()
+    director.window.push_handlers(keyboard)
+    
     menu = MainMenu()
     main_bg = MainScene()
     scene = Scene()
-    
+
     scene.add(main_bg)
     scene.add(menu)
     
